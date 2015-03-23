2e399ca80dfa1c63563e8c40e881aad15f8a61db
Eric Levin <ericrius1@gmail.com>
2015-03-23 11:10:40 -0700

    adding wall

--- lavalamp.js
+++ lavalamp.js
@@ -12,34 +12,37 @@
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
 
+
+
+
+var center = Vec3.sum(MyAvatar.position, Vec3.multiply(3.0, Quat.getFront(Camera.getOrientation())));
+
+
 var BALL_SIZE = 0.07;
-var WALL_THICKNESS = 0.10;
-var SCALE = 1.0; 
-
-var GRAVITY = -1.0;
-var LIFETIME = 600;
-var DAMPING = 0.50;
-
-var TWO_PI = 2.0 * Math.PI;
-
-var center = Vec3.sum(MyAvatar.position, Vec3.multiply(SCALE * 3.0, Quat.getFront(Camera.getOrientation())));
-
-
-
 var NUM_BALLS = 3; 
 
 balls = [];
+var wall;
 
 for (var i = 0; i < NUM_BALLS; i++) {
   balls.push(Entities.addEntity(
         { type: "Sphere",
-          position: { x: center.x + (Math.random() - 0.5) * (SCALE - BALL_SIZE - WALL_THICKNESS), 
-                y: center.y + (Math.random() - 0.5) * (SCALE - BALL_SIZE - WALL_THICKNESS) , 
-                z: center.z + (Math.random() - 0.5) * (SCALE - BALL_SIZE - WALL_THICKNESS) },  
+          position: { x: center.x + Math.random() - 0.5, 
+                y: center.y + Math.random() - 0.5 , 
+                z: center.z + Math.random() + 1},  
       dimensions: { x: BALL_SIZE, y: BALL_SIZE, z: BALL_SIZE }, 
           color: { red: Math.random() * 255, green: Math.random() * 255, blue: Math.random() * 255 },
-        })); 
+    })); 
 }
+
+//add wall
+wall = Entities.addEntity(
+{
+  type: "Box",
+  position: {x: center.x, y:center.y, z: center.z + 3},
+  dimensions: {x: 1, y: 1, z: .01},
+  color: {red: 100, green: 1, blue: 10}
+});
 
 function update(deltaTime) { 
 
@@ -53,7 +56,8 @@ function update(deltaTime) {
  
 
 function scriptEnding() {
-
+  print(wall);
+   Entities.deleteEntity(wall);
   for (var i = 0; i < NUM_BALLS; i++) {
     Entities.deleteEntity(balls[i]);
   }
