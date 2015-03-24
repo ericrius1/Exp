44905d857588a3e44aaf5da456b30d1fb713dbf7
Eric Levin <ericrius1@gmail.com>
2015-03-24 11:00:37 -0700

    spawning blob inside sphere

--- lavalamp.js
+++ lavalamp.js
@@ -1,9 +1,40 @@
 (function() {
     this.entityID = null;
+    this.lampOn = false;
+    this.numGlobs = 3;
+    this.globSize = 0.02;
+
+    
+    this.toggleLamp = function(){
+      if(!this.lampOn){
+        this.turnLampOn();
+      } else{
+        this.turnLampOff();
+      }
+
+      this.lampOn = !this.lampOn;
+    }
+
+    this.turnLampOn = function(){
+      //create blobs
+      print("BLOBS");
+      var position = Entities.getEntityProperties(this.entityID).position;
+      var globProperties = {
+        type: 'Sphere',
+        position: position,
+        color: {red: 100, blue : 20, green: 100},
+        dimensions: {x : this.globSize, y: this.globSize, z: this.globSize }
+      };
+      Entities.addEntity(globProperties);
+
+
+    }
 
     this.clickReleaseOnEntity = function(entityID, mouseEvent) {
         this.entityID = entityID;
-        print('CLICK ON ENEITYT PRITNRNRNRNRNRNRN')
+        if(mouseEvent.isLeftButton) {
+          this.toggleLamp();
+        }
      
     };
 })
