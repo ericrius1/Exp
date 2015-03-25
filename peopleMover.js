0d3a9f1146b882174c2c772ae328f5645c9e25ad
Eric Levin <ericrius1@gmail.com>
2015-03-24 19:58:04 -0700

    basic range

--- peopleMover.js
+++ peopleMover.js
@@ -1,5 +1,7 @@
 (function(){
 	this.moverOn = false
+	this.MAX_RANGE = 10;
+	var self = this;
 	this.toggleMover = function(){
 		if(!this.moverOn){
 			this.turnMoverOn();
@@ -24,10 +26,19 @@
 		this.light = Entities.addEntity({
 			type: "Light",
 			position: this.moverPosition,
+			isSpotlight: false,
 			dimensions: {x: 10, y:10, z:10},
 			color: {red: 200, green: 10, blue: 200},
 			intensity: 5
 		})
+
+		// this.debugMesh = Entities.addEntity({
+		// 	type: "Sphere",
+		// 	position: this.moverPosition,
+		// 	dimensions: {x : 5, y: 5, z:5},
+		// 	color: {red: 0, green: 100, blue: 0}
+		// })
+
 
 	}
 
@@ -39,10 +50,22 @@
 		this.cleanUp();
 	}
 
+	this.update = function(deltaTime){
+		if(!self.moverOn){
+			return;
+		}
+		print(Vec3.distance(MyAvatar.position, self.moverPosition));
+		if(Vec3.distance(MyAvatar.position, self.moverPosition) < self.MAX_RANGE){
+			print("WITHIN RANGE!");
+		}
+
+	}
+
 	this.cleanUp = function(){
 		Entities.deleteEntity(this.light);
 	}
 
 	Script.scriptEnding.connect(this.scriptEnding);
+	Script.update.connect(this.update);
 
 });
