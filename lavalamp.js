3a95cb0d6a55732510add7c093d98141fa021a30
Eric Levin <ericrius1@gmail.com>
2015-03-24 18:44:15 -0700

    in progress; '

--- lavalamp.js
+++ lavalamp.js
@@ -1,17 +1,16 @@
-var myLight;
 
 (function() {
     this.entityID = null;
     this.lampOn = false;
-    this.numGlobs = 2;
+    this.numGlobs = 1;
     this.globSize = 0.02;
     this.globs = [];
     this.lampRadius = .043;
     var self = this;
     this.colliders = [];
-    this.chanceOfVelocityBoost = 0.01;
     this.lampHeight = .5;
     this.wallSpace = 0.03;
+    this.velocityMag = 1;
 
     
     this.toggleLamp = function(){
@@ -28,18 +27,16 @@ var myLight;
     this.turnLampOn = function() {
       //create blobs
       this.lampPosition = Entities.getEntityProperties(this.entityID).position;
-      var globColor = {red: 200, blue : 20, green: 200}
+      var globColor = {red: 200, green : 20, blue: 200}
       var globProperties = {
         type: 'Sphere',
         position: this.lampPosition,
         color: globColor,
         dimensions: {x : this.globSize, y: this.globSize, z: this.globSize },
         collisionsWillMove: true,
-        velocity: {x : 0, y: 0.1, z: 0},
+        velocity: {x : -.1, y: 0.1, z: randFloat(-.1, .1)},
         damping: 0
       };
-
-     
 
       //create blobs
       for( var i = 0; i < this.numGlobs; i++){
@@ -57,6 +54,8 @@ var myLight;
           intensity: 5,
         })
 
+        globColor = {red: 0, green: 10, blue: 200};
+        globProperties.color = globColor
 
         this.globs.push(glob);
       }
@@ -72,32 +71,34 @@ var myLight;
       var colliderWidth = 0.03;
       var isVisible = false;
       var color = {red: 200, green: 10, blue: 10};
+      this.topPosition = Vec3.sum(this.lampPosition, {x: 0, y: .15, z: 0});
       var topProperties = {
         type: 'Box',
-        position: Vec3.sum(this.lampPosition, {x: 0, y: .15, z: 0}),
+        position: this.topPosition,
         dimensions: {x: this.lampRadius * 2, y: colliderWidth, z: this.lampRadius * 2},
         color: color,
         visible: isVisible
       }
       var collider = Entities.addEntity(topProperties);
       this.colliders.push(collider);
-      
+      this.bottomPosition = Vec3.sum(this.lampPosition, {x: 0, y: -.04, z: 0});
       var bottomProperties = {
         type: 'Box',
-        position: Vec3.sum(this.lampPosition, {x: 0, y: -.04, z: 0}),
+        position: this.bottomPosition,
         dimensions: {x: this.lampRadius * 2, y: colliderWidth, z: this.lampRadius * 2},
         color: color,
         visible: isVisible
       }
       collider = Entities.addEntity(bottomProperties);
       this.colliders.push(collider);
+      this.midPosition = (this.topPosition.y - this.bottomPosition.y)/2;
       
       //SIDE WALLS
        var wall = Entities.addEntity(
         { type: "Box",
           position: Vec3.sum(this.lampPosition, { x:this.wallSpace, y: 0, z: 0 }), 
           dimensions: { x: colliderWidth, y: this.lampHeight, z: this.wallSpace * 2 }, 
-          color: { red: 0, green: 255, blue: 0 },
+          color: color,
           ignoreCollisions: false,
           visible: isVisible,
         });
@@ -107,7 +108,7 @@ var myLight;
         { type: "Box",
           position: Vec3.subtract(this.lampPosition, { x: this.wallSpace, y: 0, z: 0 }), 
           dimensions: { x: colliderWidth, y: this.lampHeight, z: this.wallSpace * 2 }, 
-          color: { red: 0, green: 255, blue: 0 },
+          color: color,
           ignoreCollisions: false,
           visible: isVisible,
         });
@@ -116,7 +117,7 @@ var myLight;
         { type: "Box",
           position: Vec3.subtract(this.lampPosition, { x: 0, y: 0, z: this.wallSpace}), 
           dimensions: { x: this.wallSpace * 2, y: this.lampHeight, z: colliderWidth }, 
-          color: { red: 0, green: 255, blue: 0 },
+          color: color,
           ignoreCollisions: false,
           visible: isVisible,
          }); 
@@ -126,7 +127,7 @@ var myLight;
         { type: "Box",
           position: Vec3.sum(this.lampPosition, { x: 0, y: 0, z: this.wallSpace}), 
           dimensions: { x: this.wallSpace * 2, y: this.lampHeight, z: colliderWidth }, 
-          color: { red: 0, green: 255, blue: 0 },
+          color: color,
           ignoreCollisions: false,
           visible: isVisible,
         });
@@ -152,16 +153,20 @@ var myLight;
       }
 
       for(var i = 0; i < self.globs.length; i++){
-        var glob = self.globs[0];
-        var velocity = Entities.getEntityProperties(glob).velocity;
-        var position = Entities.getEntityProperties(glob).position;
+        glob = self.globs[0];
+        velocity = Entities.getEntityProperties(glob).velocity;
+        position = Entities.getEntityProperties(glob).position;
 
         //Physics engine deactivates objects moving below 0.05 m/s after ~2 sec, 
         //so give those objects a boost before that happens
-        if(Vec3.length(velocity) < 0.05){
-          // var newVelocity = Vec3.multiply(2, velocity);
-          var direction = velocity.y > 0 ? 1 : -1;
-          Entities.editEntity(glob, {velocity: {x: 0, y: .1 * direction, z: 0}});
+        if(Vec3.length(velocity) <= 0.1){
+          if(position.y > self.midPosition){
+            newYVelocity = -.101;
+          } else{
+            newYVelocity = .101;
+          }
+          Entities.editEntity(glob, {velocity: {x: 0, y: newYVelocity, z: 0}});
+
         }
         Entities.editEntity(glob.light, {position: position});
 
