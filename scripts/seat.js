c9df9ba2d230fc68b7ee459a5622d1340163a3f1
Eric Levin <ericrius1@gmail.com>
2015-03-30 16:14:14 -0700

    moving to object

--- scripts/seat.js
+++ scripts/seat.js
@@ -5,6 +5,8 @@
     this.entityId = entityId;
     this.properties = Entities.getEntityProperties(this.entityId);
 		this.isSittingSettingHandle = "AvatarSittingState";
+    Settings.setValue(this.isSittingSettingHandle, false);
+
 	}
 
 	this.clickReleaseOnEntity = function(entityId, mouseEvent){
@@ -12,39 +14,45 @@
      
       if(Settings.getValue(this.isSittingSettingHandle, false) == "false"){
         //first we need to move avatar towards chair
-        Script.update.connect(this.goToSeat);
-        this.activeUpdateFunction = this.goToSeat;
+        this.activeUpdate = this.moveToSeat;
       }
-		}
-	}
-
-	this.goToSeat = function(){
-    self.distance = Vec3.distance(MyAvatar.position, self.properties.position)
-    print(self.distance)
-    if(self.distance > self.targetAvatarToChairDistance){
-      MyAvatar.position = Vec3.mix(MyAvatar.position, self.properties.position, 0.1);
-    } else {
-      //otherwise we made it ot chair, now sit down
-      Script.update.disconnect(self.goToSeat);
-      Script.update.connect(self.sitDown);
-      self.activeUpdateFunction = self.sitDown;
     }
-	}
+  }
+
+  this.update = function(){
+    if(!self.activeUpdate){
+      return;
+    }
+    self.activeUpdate();
+   
+  }
+
+  this.moveToSeat = function(){
+    self.distance = Vec3.distance(MyAvatar.position, self.properties.position)
+    if(self.distance > self.targetAvatarToChairDistance){
+      MyAvatar.position = Vec3.mix(MyAvatar.position, self.properties.position, 0.01);
+    } else {
+      //otherwise we made it ot chair, now sit down should be out active update function
+      self.activeUpdate = self.sitDown;
+    }
+
+  }
 
   this.sitDown = function(){
-    print("YAAAAAA")
+    
 
   }
 
   this.unload = function(){
 
-    Script.update.disconnect(this.sitDown);
+    Script.update.disconnect(this.update);
   }
 
   function map(value, min1, max1, min2, max2) {
     return min2 + (max2 - min2) * ((value - min1) / (max1 - min1));
   }
 
+  Script.update.connect(this.update);
 
 
 });
