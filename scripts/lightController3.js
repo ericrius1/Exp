770d19a3666df4297831246ba45b88593bc56f74
Eric Levin <ericrius1@gmail.com>
2015-04-01 14:13:28 -0700

    sound;

--- scripts/lightController3.js
+++ scripts/lightController3.js
@@ -1,7 +1,14 @@
 (function() {
 
   this.preload = function(entityId) {
+    this.sound = SoundCache.getSound("https://hifi-public.s3.amazonaws.com/sounds/Switches%20and%20sliders/lamp_switch_1.wav");
     this.entityId = entityId;
+    this.properties = Entities.getEntityProperties(this.entityId);
+    this.getUserData()
+    if (!this.userData) {
+      this.userData = {};
+      this.userData.lightOn = false;
+    }
 
   }
 
@@ -19,20 +26,45 @@
   }
 
   this.clickReleaseOnEntity = function(entityId, mouseEvent) {
+    if (!mouseEvent.isLeftButton) {
+      return;
+    }
     //firist find closest light 
     this.entityId = entityId
+    this.playSound();
     this.properties = Entities.getEntityProperties(this.entityId)
     this.light = this.findClosestLight();
+    if (this.light) {
+      this.getUserData();
+      Entities.editEntity(this.light, {
+        visible: !this.userData.lightOn
+      });
+
+      this.userData.lightOn = !this.userData.lightOn;
+      this.updateUserData();
+    }
   }
 
+  this.playSound = function() {
+    if (this.sound && this.sound.downloaded) {
+      Audio.playSound(this.sound, {
+        position: this.properties.position,
+        volume: 0.3
+      });
+    } else {
+      print("Warning: Couldn't play sound.");
+    }
+  }
+
+
   this.findClosestLight = function() {
-    var entities = Entities.findEntities(this.properties.position, 1);
+    var entities = Entities.findEntities(this.properties.position, 0.5);
     for (var i = 0; i < entities.length; i++) {
       if (Entities.getEntityProperties(entities[i]).type === "Light") {
         return entities[i];
       }
     }
-    return [];
+    return null;
   }
 
 });
