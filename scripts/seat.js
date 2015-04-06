f320f1bd2d574ce078880086f83a2c4274918605
Eric Levin <ericrius1@gmail.com>
2015-04-06 12:27:04 -0700

    sitting at seat height

--- scripts/seat.js
+++ scripts/seat.js
@@ -1,64 +1,121 @@
-(function(){
-	var self = this;
-	this.preload = function(entityId){
+(function() {
+  var self = this;
+  this.preload = function(entityId) {
     this.buttonImageURL = "https://s3.amazonaws.com/hifi-public/images/tools/sit.svg";
     this.addStandButton();
     this.totalAnimationTime = 0.7;
     this.targetAvatarToChairDistance = 0.5;
     this.entityId = entityId;
     this.properties = Entities.getEntityProperties(this.entityId);
-		this.isSittingSettingHandle = "AvatarSittingState";
+    this.isSittingSettingHandle = "AvatarSittingState";
     Settings.setValue(this.isSittingSettingHandle, false);
     this.startPoseAndTransition = [];
-    
-    this.seatVelocity = {x: -.01, y: 0, z: 0};
-    this.radius = this.properties.dimensions.x/2;
+    this.forward = {x: 0, y: 0, z: -1};
+
+    this.seatVelocity = {
+      x: -.01,
+      y: 0,
+      z: 0
+    };
+    this.radius = this.properties.dimensions.x / 2;
     this.sittingAreaAngle = Math.PI;
     this.numSeats = 5;
-    this.seatHeight = 1;
+    this.seatHeight = 0.0;
 
     //target pose
     this.pose = [
-      {joint:"RightUpLeg", rotation: {x:100.0, y:15.0, z:0.0}},
-      {joint:"RightLeg", rotation: {x:-130.0, y:15.0, z:0.0}},
-      {joint:"RightFoot", rotation: {x:30, y:15.0, z:0.0}},
-      {joint:"LeftUpLeg", rotation: {x:100.0, y:-15.0, z:0.0}},
-      {joint:"LeftLeg", rotation: {x:-130.0, y:-15.0, z:0.0}},
-      {joint:"LeftFoot", rotation: {x:30, y:15.0, z:0.0}}
+      {
+        joint: "RightUpLeg",
+        rotation: {
+          x: 100.0,
+          y: 15.0,
+          z: 0.0
+        }
+      },
+      {
+        joint: "RightLeg",
+        rotation: {
+          x: -130.0,
+          y: 15.0,
+          z: 0.0
+        }
+      },
+      {
+        joint: "RightFoot",
+        rotation: {
+          x: 30,
+          y: 15.0,
+          z: 0.0
+        }
+      },
+      {
+        joint: "LeftUpLeg",
+        rotation: {
+          x: 100.0,
+          y: -15.0,
+          z: 0.0
+        }
+      },
+      {
+        joint: "LeftLeg",
+        rotation: {
+          x: -130.0,
+          y: -15.0,
+          z: 0.0
+        }
+      },
+      {
+        joint: "LeftFoot",
+        rotation: {
+          x: 30,
+          y: 15.0,
+          z: 0.0
+        }
+      }
     ]
 
     this.storeStartPoseAndTransition();
     this.getUserData();
-	}
+  }
 
   this.getUserData = function() {
-    if(this.properties.userData){
+    if (this.properties.userData) {
       this.userData = JSON.parse(this.properties.userData);
-    } else{
+    } else {
       this.userData = {};
     }
-    if(!this.userData.currentSeatIndex){
+    if (!this.userData.currentSeatIndex) {
       this.userData.currentSeatIndex = 0;
       this.updateUserData();
     }
   }
 
-  this.updateUserData = function(){
-    Entities.editEntity(this.entityId, {userData: JSON.stringify(this.userData) });
+  this.updateUserData = function() {
+    Entities.editEntity(this.entityId, {
+      userData: JSON.stringify(this.userData)
+    });
   }
-  
 
-  this.addStandButton = function(){
+
+  this.addStandButton = function() {
     this.windowDimensions = Controller.getViewportDimensions();
     this.buttonWidth = 37;
     this.buttonHeight = 46;
     this.buttonPadding = 10;
 
     this.buttonPositionX = this.windowDimensions.x - this.buttonPadding - this.buttonWidth;
-    this.buttonPositionY = (this.windowDimensions.y - this.buttonHeight)/2 - (this.buttonHeight + this.buttonPadding);
+    this.buttonPositionY = (this.windowDimensions.y - this.buttonHeight) / 2 - (this.buttonHeight + this.buttonPadding);
     this.standUpButton = Overlays.addOverlay("image", {
-      x: this.buttonPositionX, y: this.buttonPositionY, width: this.buttonWidth, height: this.buttonHeight,
-      subImage: {x : this.buttonWidth, y: this.buttonHeight, width: this.buttonWidth, height: this.buttonHeight},
+      x: this.buttonPositionX,
+      y: this.buttonPositionY,
+      width: this.buttonWidth,
+      height: this.buttonHeight,
+      subImage: {
+        x: this.buttonWidth,
+        y: this.buttonHeight,
+        width: this.buttonWidth,
+        height: this.buttonHeight
+      },
       imageURL: this.buttonImageURL,
       visible: false,
       alpha: 1.0
@@ -66,51 +123,63 @@
   }
 
   this.storeStartPoseAndTransition = function() {
-    for(var i = 0; i < this.pose.length; i++){
+    for (var i = 0; i < this.pose.length; i++) {
       var startRotation = Quat.safeEulerAngles(MyAvatar.getJointRotation(this.pose[i].joint));
       var transitionVector = Vec3.subtract(this.pose[i].rotation, startRotation);
-      this.startPoseAndTransition.push({joint: this.pose[i].joint, start: startRotation, transition: transitionVector});
+      this.startPoseAndTransition.push({
+        joint: this.pose[i].joint,
+        start: startRotation,
+        transition: transitionVector
+      });
     }
   }
 
-	this.clickReleaseOnEntity = function(entityId, mouseEvent){
-		if(mouseEvent.isLeftButton){
-     
-      if(Settings.getValue(this.isSittingSettingHandle, false) == "false"){
-        this.getUserData();
-        this.currentSeatIndex = this.userData.currentSeatIndex;
-        this.userData.currentSeatIndex++;
-        this.updateUserData();
-        //first we need to move avatar towards chair
-        this.activeUpdate = this.moveToSeat;
+  this.clickReleaseOnEntity = function(entityId, mouseEvent) {
+    if (mouseEvent.isLeftButton) {
+
+      if (Settings.getValue(this.isSittingSettingHandle, false) == "false") {
+        this.initMoveToSeat();
+
       }
     }
   }
 
-  this.update = function(deltaTime){
-    print('waaah')
-    if(self.entityId){
+  this.initMoveToSeat = function() {
+    this.getUserData();
+    this.currentSeatIndex = this.userData.currentSeatIndex;
+    this.userData.currentSeatIndex++;
+    this.updateUserData();
+    this.seatTheta = -this.currentSeatIndex / this.numSeats * this.sittingAreaAngle;
+    //first we need to move avatar towards chair
+    this.activeUpdate = this.moveToSeat;
+
+  }
+
+  this.update = function(deltaTime) {
+    if (self.entityId) {
       self.properties = Entities.getEntityProperties(self.entityId);
       //need to always update seat so when user clicks on it it is in proper world space
-      var theta = -self.currentSeatIndex/self.numSeats * self.sittingAreaAngle;
-      var xPos = self.properties.position.x + (Math.cos(theta) * self.radius);
-      var zPos = self.properties.position.z + (Math.sin(theta) * self.radius);
-      self.seatPosition = {x: xPos, y: self.properties.position.y + self.seatHeight, z: zPos};
+      var xPos = self.properties.position.x + (Math.cos(self.seatTheta) * self.radius);
+      var zPos = self.properties.position.z + (Math.sin(self.seatTheta) * self.radius);
+      self.seatPosition = {
+        x: xPos,
+        y: self.properties.position.y + self.seatHeight,
+        z: zPos
+      };
     }
 
 
-    if(!self.activeUpdate){
+    if (!self.activeUpdate) {
       return;
     }
     self.activeUpdate(deltaTime);
-   
+
   }
 
-  this.moveToSeat = function(deltaTime){
+  this.moveToSeat = function(deltaTime) {
     self.distance = Vec3.distance(MyAvatar.position, self.seatPosition);
-    if(self.distance > self.targetAvatarToChairDistance){
-      self.sanitizedRotation = Quat.fromPitchYawRollDegrees(0, Quat.safeEulerAngles(self.properties.rotation).y, 0);
-      MyAvatar.orientation = Quat.mix(MyAvatar.orientation, self.sanitizedRotation, 0.02);
+    if (self.distance > self.targetAvatarToChairDistance) {
+      // self.sanitizedRotation = Quat.fromPitchYawRollDegrees(0, Quat.safeEulerAngles(self.properties.rotation).y, 0);
       MyAvatar.position = Vec3.mix(MyAvatar.position, self.seatPosition, 0.05);
     } else {
       //otherwise we made it to chair, now sit down should be out active update function
@@ -120,34 +189,34 @@
 
   }
 
-  this.sitDown = function(deltaTime){
+  this.sitDown = function(deltaTime) {
     self.elapsedTime += deltaTime;
-    self.factor = self.elapsedTime/self.totalAnimationTime;
-    if(self.elapsedTime< self.totalAnimationTime){
+    self.factor = self.elapsedTime / self.totalAnimationTime;
+    if (self.elapsedTime < self.totalAnimationTime) {
       self.updateJoints();
     } else {
       //We've sat, now we don't need to update any longer;
       self.activeUpdate = null;
       Settings.setValue(self.isSittingSettingHandle, true);
-      Overlays.editOverlay(self.standUpButton, {visible: true});
+      Overlays.editOverlay(self.standUpButton, {
+        visible: true
+      });
       var isValid = MyAvatar.setModelReferential(self.properties.id);
     }
   }
 
-  this.standUp = function(deltaTime){
+  this.standUp = function(deltaTime) {
 
     self.elapsedTime += deltaTime;
-    self.factor = 1 - self.elapsedTime/self.totalAnimationTime;
-    print("ELAPSED TIME  *****************   " + self.elapsedTime)
-    if(self.elapsedTime < self.totalAnimationTime){      
+    self.factor = 1 - self.elapsedTime / self.totalAnimationTime;
+    if (self.elapsedTime < self.totalAnimationTime) {
       self.updateJoints();
-      print("Stand up!")
     } else {
       //We're done with standing animation
       self.activeUpdate = null;
       Settings.setValue(this.isSittingSettingHandle, false);
       //We just finished a standup after deleting the entity the avatar was sitting on
-      if(self.disconnectAfterStanding){
+      if (self.disconnectAfterStanding) {
         Overlays.deleteOverlay(this.standUpButton);
         Script.update.disconnect(self.update);
       }
@@ -159,8 +228,8 @@
   }
 
 
-  self.updateJoints = function(){
-    for(var i = 0; i < self.startPoseAndTransition.length; i++){
+  self.updateJoints = function() {
+    for (var i = 0; i < self.startPoseAndTransition.length; i++) {
       self.scaledTransition = Vec3.multiply(self.startPoseAndTransition[i].transition, self.factor);
       self.jointRotation = Vec3.sum(self.startPoseAndTransition[i].start, self.scaledTransition);
       MyAvatar.setJointData(self.startPoseAndTransition[i].joint, Quat.fromVec3Degrees(self.jointRotation));
@@ -169,44 +238,49 @@
 
   }
 
-  this.clearAvatarAnimation = function(){
+  this.clearAvatarAnimation = function() {
     MyAvatar.clearReferential();
-    for(var i = 0; i < self.pose.length; i++){
+    for (var i = 0; i < self.pose.length; i++) {
       MyAvatar.clearJointData(this.pose[i].joint);
     }
-    Overlays.editOverlay(this.standUpButton, {visible: false});
+    Overlays.editOverlay(this.standUpButton, {
+      visible: false
+    });
 
   }
 
-  this.unload = function(){
+  this.unload = function() {
     var isSitting = Settings.getValue(this.isSittingSettingHandle)
     this.entityId = null;
-    if(isSitting === "true"){
-      print("SHOULD BE HERE ****************************")
-    //  We need to let avatar stand before disconnecting, and then end
+    if (isSitting === "true") {
+      //  We need to let avatar stand before disconnecting, and then end
       this.initStandUp();
       this.disconnectAfterStanding = true;
       this.activeUpdate = this.standUp
     } else {
       this.clearAvatarAnimation();
-      print("DISCONNECT")
       Script.update.disconnect(this.update);
     }
   }
 
-  this.onClick = function(event){
-    var clickedOverlay = Overlays.getOverlayAtPoint({x: event.x, y: event.y});
-    if(clickedOverlay === self.standUpButton){
-     self.initStandUp();
+  this.onClick = function(event) {
+    var clickedOverlay = Overlays.getOverlayAtPoint({
+      x: event.x,
+      y: event.y
+    });
+    if (clickedOverlay === self.standUpButton) {
+      self.initStandUp();
     }
 
   }
 
-  this.initStandUp = function(){
+  this.initStandUp = function() {
     this.elapsedTime = 0;
     this.activeUpdate = this.standUp;
     MyAvatar.clearReferential();
-    Overlays.editOverlay(self.standUpButton, {visible: false});
+    Overlays.editOverlay(self.standUpButton, {
+      visible: false
+    });
   }
 
   function map(value, min1, max1, min2, max2) {
