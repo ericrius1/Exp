e72da8dcf4b81db0b43ec4323c70559f1b1f7604
Eric Levin <ericrius1@gmail.com>
2015-04-07 14:25:56 -0700

    going to add spring force

--- scripts/hitTarget.js
+++ scripts/hitTarget.js
@@ -2,30 +2,35 @@ var controllerID, controllerActive, holdPosition;
 var originalTargetPosition = Vec3.sum(MyAvatar.position, Vec3.multiply(1.2, Quat.getFront(Camera.getOrientation())));
 originalTargetPosition.y = MyAvatar.position.y;
 var dPosition;
-var properties;
+var stickProperties, spring, springLength, targetVelocity;
+var HOLD_POSITION_OFFSET = {x: 0, y: 0, z: -0.5};
+var originalStickPosition = {x: originalTargetPosition.x + 0.5, y: originalTargetPosition.y, z: originalTargetPosition.z};
+var STICK_ORIENTATION = Quat.fromPitchYawRollDegrees(0, 0, 0);
+var SPRING_FORCE = 15.0;
+
+
 var target = Entities.addEntity({
-	type: "Box",
+  type: "Box",
   position: originalTargetPosition,
-  dimensions: {x: .2, y: 0.2, z: 0.2},
+  dimensions: {x: .1, y: 0.1, z: 0.1},
   color: {red: 200, green: 20, blue: 200},
   rotation: MyAvatar.orientation,
   ignoreCollisions: false,
   collisionsWillMove: true,
+  damping: 0.0
 });
 
-var HOLD_POSITION_OFFSET = {x: 0, y: 0, z: -0.5};
-var originalStickPosition = {x: originalTargetPosition.x + 0.5, y: originalTargetPosition.y, z: originalTargetPosition.z};
-STICK_ORIENTATION = Quat.fromPitchYawRollDegrees(0, 0, 0);
 print("STICK_ORIENTATION  ****************  " + JSON.stringify(STICK_ORIENTATION));
 var stickWorldOrientation;
 var stick = Entities.addEntity({
   type: "Box",
   position: originalStickPosition,
-  dimensions: {x: .1, y:.1, z: 0.5},
+  dimensions: {x: .2, y:.2, z: 0.5},
   rotation: MyAvatar.orientation,
   color: {red: 200, blue: 10, green: 10},
   ignoreCollisions: false,
-  collisionsWillMove: true
+  collisionsWillMove: true,
+  damping: 0
 })
 
 initControls();
@@ -64,6 +69,7 @@ function update(delta){
     return;
   }
   var holdPosition = Vec3.sum(MyAvatar.getRightPalmPosition(), Vec3.multiplyQbyV(MyAvatar.orientation, HOLD_POSITION_OFFSET));
+  stickProperties = Entities.getEntityProperties(stick);
   stickWorldOrientation = Quat.multiply(Quat.multiply(MyAvatar.orientation, Controller.getSpatialControlRawRotation(controllerID)), STICK_ORIENTATION);
   Entities.editEntity(stick, {position: holdPosition, rotation: stickWorldOrientation });
 }
