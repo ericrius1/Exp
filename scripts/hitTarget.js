4dab7c75a030d82591919515b780905926792ae9
Eric Levin <ericrius1@gmail.com>
2015-04-08 11:11:57 -0700

    invisible sword box

--- scripts/hitTarget.js
+++ scripts/hitTarget.js
@@ -4,8 +4,8 @@ originalTargetPosition.y = MyAvatar.position.y;
 var dPosition;
 var stickProperties, spring, springLength, targetVelocity;
 var HOLD_POSITION_OFFSET = {x: 0, y: 0, z: -0.5};
-var originalStickPosition = {x: originalTargetPosition.x + 0.5, y: originalTargetPosition.y, z: originalTargetPosition.z};
-var STICK_ORIENTATION = Quat.fromPitchYawRollDegrees(0, 0, 0);
+var originalSwordPosition = {x: originalTargetPosition.x + 0.5, y: originalTargetPosition.y, z: originalTargetPosition.z};
+var SWORD_ORIENTATION = Quat.fromPitchYawRollDegrees(0, 0, 0);
 var SPRING_FORCE = 15.0;
 
 
@@ -20,19 +20,27 @@ var target = Entities.addEntity({
   damping: 0.3
 });
 
-print("STICK_ORIENTATION  ****************  " + JSON.stringify(STICK_ORIENTATION));
-var stickWorldOrientation;
-var stick = Entities.addEntity({
+var swordWorldOrientation;
+var sword = Entities.addEntity({
   type: "Model",
   modelURL: "https://hifi-public.s3.amazonaws.com/eric/models/sword2.fbx",
-  position: originalStickPosition,
+  position: originalSwordPosition,
   dimensions: {x: .05, y:.02, z: 0.26},
   rotation: MyAvatar.orientation,
   color: {red: 200, blue: 10, green: 10},
-  ignoreCollisions: false,
-  collisionsWillMove: true,
   damping: 0
 });
+
+var swordCollisionBox = Entities.addEntity({
+  type: "Box",
+  position: originalSwordPosition,
+  dimensions: {x: .05, y: 0.02, z: 0.26},
+  color: {red: 200, blue: 10, green: 10},
+  rotation: MyAvatar.orientation,
+  ignoreCollisions: false,
+  collisionsWillMove: true,
+  visible: false
+})
 
 initControls();
 
@@ -40,28 +48,30 @@ function initControls(){
   controllerID = 3; //right handed
   var palmPosition = Controller.getSpatialControlPosition(controllerID);
   controllerActive = (Vec3.length(palmPosition) > 0);
-  
 }
 
 Script.setTimeout(function(){
   target = Entities.identifyEntity(target);
-  stick = Entities.identifyEntity(stick);  
+  swordCollisionBox = Entities.identifyEntity(swordCollisionBox);  
 }, 500);
 
 function cleanUp(){
   Entities.deleteEntity(target);
-  Entities.deleteEntity(stick);
+  Entities.deleteEntity(sword);
+  Entities.deleteEntity(swordCollisionBox);
 }
 
 
 function onCollision(entity1, entity2, collision){
-  print('collision');
+  if(entity1.id === swordCollisionBox.id || entity2.id === swordCollisionBox.id){
+    print("COLLISION!")
+  }
 }
 
 function onKeyPress(event){
   if(event.text === 'f'){
     Entities.editEntity(target, {position: originalTargetPosition, rotation: MyAvatar.orientation, velocity: {x: 0, y: 0, z: 0}, angularVelocity: {x: 0, y: 0, z:0}});
-    Entities.editEntity(stick, {position: originalStickPosition, rotation: MyAvatar.orientation, velocity: {x: 0, y:0, z: 0}, angularVelocity: {x: 0, y: 0, z: 0}});
+    Entities.editEntity(sword, {position: originalSwordPosition, rotation: MyAvatar.orientation, velocity: {x: 0, y:0, z: 0}, angularVelocity: {x: 0, y: 0, z: 0}});
   }
 }
 
@@ -71,22 +81,20 @@ function update(deltaTime){
   }
   holdPosition = Vec3.sum(MyAvatar.getRightPalmPosition(), Vec3.multiplyQbyV(MyAvatar.orientation, HOLD_POSITION_OFFSET));
   targetProperties = Entities.getEntityProperties(target);
-  // if(Math.random() > .9){
-  //   print("ANGULAR VELOCITY **************** " + targetProperties.angularVelocity.x);
-  // }
+
   spring = Vec3.subtract(originalTargetPosition, targetProperties.position);
   springLength = Vec3.length(spring);
   spring = Vec3.normalize(spring);
   targetVelocity = Vec3.sum(targetProperties.velocity, Vec3.multiply(springLength * SPRING_FORCE * deltaTime, spring));
-  stickWorldOrientation = Quat.multiply(Quat.multiply(MyAvatar.orientation, Controller.getSpatialControlRawRotation(controllerID)), STICK_ORIENTATION);
-  Entities.editEntity(stick, {position: holdPosition, rotation: stickWorldOrientation });
+  swordWorldOrientation = Quat.multiply(Quat.multiply(MyAvatar.orientation, Controller.getSpatialControlRawRotation(controllerID)), SWORD_ORIENTATION);
+  Entities.editEntity(sword, {position: holdPosition, rotation: swordWorldOrientation });
+  Entities.editEntity(swordCollisionBox, {position: holdPosition, rotation: swordWorldOrientation });
   Entities.editEntity(target, {velocity: targetVelocity});
-  // Entities.editEntity(target, {position: originalTargetPosition});
 }
 
 Controller.keyPressEvent.connect(onKeyPress);
 
-// Entities.entityCollisionWithEntity.connect(onCollision);
+Entities.entityCollisionWithEntity.connect(onCollision);
 
 Script.scriptEnding.connect(cleanUp);
 Script.update.connect(update);
