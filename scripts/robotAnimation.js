a95658f7fccb3ded8c5e35c5243a5a1032456b3c
Eric Levin <ericrius1@gmail.com>
2015-04-20 16:23:45 -0700

    rotating right wat

--- scripts/robotAnimation.js
+++ scripts/robotAnimation.js
@@ -13,7 +13,8 @@ var forward, velocity, normalizedVelocity, targetRotation, rotation, targetLine,
 var angleOffset = 0;
 var angleDirection;
 var test = 0;
-
+var avatarYaw = MyAvatar.bodyYaw;
+var previousAvatarYaw = avatarYaw;
 var previousAvatarOrientation = MyAvatar.orientation;
 var previousPivotRotation = pivotStartRotation;
 var targetPivotRotation = pivotStartRotation;
@@ -90,23 +91,30 @@ function updateDebugLines() {
 
 
 function setNewTargetPivot(){
+  avatarYaw = MyAvatar.bodyYaw;
   var avatarForward = Vec3.sum(MyAvatar.position, Vec3.multiply(lineLength, Quat.getFront(MyAvatar.orientation)));
   var previousAvatarForward = Vec3.sum(MyAvatar.position, Vec3.multiply(lineLength, Quat.getFront(previousAvatarOrientation)));
   Overlays.editOverlay(targetLine, {start: MyAvatar.position, end: avatarForward});
   Overlays.editOverlay(startLine, {start: MyAvatar.position, end: previousAvatarForward});
   avatarForward = Vec3.normalize(avatarForward);
   previousAvatarForward = Vec3.normalize(previousAvatarForward);
-  angleDirection = Math.atan2(avatarForward.y, avatarForward.x) - Math.atan2(previousAvatarForward.y, previousAvatarForward.x);
-  print("DIRECTION" + angleDirection)
+  angleDirection = Math.atan2(avatarForward.y, avatarForward.z) - Math.atan2(previousAvatarForward.y, previousAvatarForward.z);
   angleOffset = Math.acos(Math.min(1, Vec3.dot(Quat.getFront(MyAvatar.orientation), Quat.getFront(previousAvatarOrientation))));
   angleDirection > 0 ? angleOffset *=-1 : angleOffset *= 1;
+
+  //we need to account for angle changes between the poles and un-reverse direction
+  if(avatarYaw > 0 && avatarYaw < 180){
+    print("OFFSET CHAE")
+    angleOffset *= -1;
+  }
   angleOffset *=57;
   // print("AVATAR YAW" + Quat.safeEulerAngles(MyAvatar.orientation).y);
   print("ANGLE " + angleOffset);
   previousAvatarOrientation = MyAvatar.orientation;
+  previousAvatarYaw = avatarYaw;
   //rotate yaw of pivot by angle offset
   var safeAngle = {x: eulerPivotStartRotation.x, y: eulerPivotStartRotation.y, z: eulerPivotStartRotation.z};
-  print(eulerPivotStartRotation.y);
+  // print(eulerPivotStartRotation.y);
   safeAngle.y += angleOffset;
   MyAvatar.setJointData(pivotJoint, Quat.fromVec3Degrees(safeAngle));
 }
