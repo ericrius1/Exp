fc468bb2d5734b566bc581242280ef8153d2293c
Eric Levin <ericrius1@gmail.com>
2015-04-28 18:46:39 -0700

    added requested functionality to grab script

--- scripts/mouseGrab2.js
+++ scripts/mouseGrab2.js
@@ -14,14 +14,133 @@ var prevPosition;
 var newPosition;
 var flingVelocity;
 var flingMultiplier = 10;
-var DAMPING = 0.95;
-var startingHeight;
 var moveUpDown = false;
 var savedGravity;
+
+var DROP_DISTANCE = 5.0;
+var DROP_COLOR = {
+  red: 200,
+  green: 200,
+  blue: 200
+};
+var DROP_WIDTH = 4;
 
 
 var autoBox = true;
 if (autoBox) {
+  setUpTestObjects();
+}
+
+var dropLine = Overlays.addOverlay("line3d", {
+  start: {
+    x: 0,
+    y: 0,
+    z: 0
+  },
+  end: {
+    x: 0,
+    y: 0,
+    z: 0
+  },
+  color: DROP_COLOR,
+  alpha: 1,
+  visible: false,
+  lineWidth: DROP_WIDTH
+});
+
+
+function mousePressEvent(event) {
+  var pickRay = Camera.computePickRay(event.x, event.y);
+  var intersection = Entities.findRayIntersection(pickRay);
+  if (intersection.intersects && intersection.properties.collisionsWillMove) {
+    grabbedEntity = intersection.entityID;
+    var props = Entities.getEntityProperties(grabbedEntity)
+    prevPosition = props.position;
+    isGrabbing = true;
+    savedGravity = props.gravity;
+    Overlays.editOverlay(dropLine, {visible: true});
+    Entities.editEntity(grabbedEntity, {
+      gravity: {
+        x: 0,
+        y: 0,
+        z: 0
+      }
+    });
+    //We need to store entity's current gravity, and then disable it while we move
+
+  }
+
+}
+
+
+function mouseReleaseEvent() {
+  if (isGrabbing) {
+    flingObject();
+    Entities.editEntity(grabbedEntity, {
+      gravity: savedGravity
+    });
+  }
+  isGrabbing = false;
+  Overlays.editOverlay(dropLine, {visible: false});
+}
+
+function flingObject() {
+  //calculate velocity to give object base on current and previous position
+  entityProps = Entities.getEntityProperties(grabbedEntity);
+
+  flingVelocity = Vec3.subtract(entityProps.position, prevPosition);
+  flingVelocity = Vec3.multiply(flingMultiplier, flingVelocity);
+  flingVelocity.y = 0;
+  Entities.editEntity(grabbedEntity, {
+    velocity: flingVelocity
+  });
+}
+
+function mouseMoveEvent(event) {
+  if (isGrabbing) {
+    entityProps = Entities.getEntityProperties(grabbedEntity);
+    prevPosition = entityProps.position;
+    avatarEntityDistance = Vec3.distance(MyAvatar.position, entityProps.position);
+    finalMoveMultiplier = baseMoveFactor * Math.pow(avatarEntityDistance, 1.5);
+    deltaMouse.x = event.x - prevMouse.x;
+    if (!moveUpDown) {
+      deltaMouse.z = event.y - prevMouse.y;
+    } else {
+      deltaMouse.y = (event.y - prevMouse.y) * -1;
+    }
+    finalMoveMultiplier = baseMoveFactor * Math.pow(avatarEntityDistance, 1.5);
+    deltaMouse = Vec3.multiply(deltaMouse, finalMoveMultiplier);
+    camYaw = Quat.safeEulerAngles(Camera.getOrientation()).y;
+    dv = Vec3.multiplyQbyV(Quat.fromPitchYawRollDegrees(0, camYaw, 0), deltaMouse);
+    newPosition = Vec3.sum(entityProps.position, dv);
+    Entities.editEntity(grabbedEntity, {
+      position: newPosition
+    });
+    Overlays.editOverlay(dropLine, {start: newPosition, end: Vec3.sum(newPosition, {x: 0, y: -DROP_DISTANCE, z: 0}) });
+  }
+  prevMouse.x = event.x;
+  prevMouse.y = event.y;
+}
+
+function keyReleaseEvent(event) {
+  if (event.text === "SHIFT") {
+    moveUpDown = false;
+  }
+}
+
+function keyPressEvent(event) {
+  if (event.text === "SHIFT") {
+    moveUpDown = true;
+  }
+}
+
+function cleanup() {
+  Entities.deleteEntity(box);
+  Entities.deleteEntity(box2);
+  Entities.deleteEntity(ground);
+}
+
+function setUpTestObjects() {
   var distance = 4;
   box = Entities.addEntity({
     type: 'Box',
@@ -37,10 +156,14 @@ if (autoBox) {
       blue: 192
     },
     collisionsWillMove: true,
-    gravity: {x: 0, y: -1, z: 0}
+    gravity: {
+      x: 0,
+      y: -1,
+      z: 0
+    }
   });
 
-   box2 = Entities.addEntity({
+  box2 = Entities.addEntity({
     type: 'Box',
     position: Vec3.sum(MyAvatar.position, Vec3.multiply(distance + 1, Quat.getFront(Camera.getOrientation()))),
     dimensions: {
@@ -54,94 +177,31 @@ if (autoBox) {
       blue: 192
     },
     collisionsWillMove: true,
-    gravity: {x: 0, y: -1, z: 0}
+    gravity: {
+      x: 0,
+      y: -1,
+      z: 0
+    }
   });
 
-   ground = Entities.addEntity({
+  ground = Entities.addEntity({
     type: 'Box',
-    position: {x: MyAvatar.position.x, y: MyAvatar.position.y - 5, z: MyAvatar.position.z},
-    dimensions: {x: 100, y: 2, z: 100},
-    color: {red: 20, green: 200, blue: 50}
-   })
-}
-
-
-function mousePressEvent(event) {
-  var pickRay = Camera.computePickRay(event.x, event.y);
-  var intersection = Entities.findRayIntersection(pickRay);
-  if (intersection.intersects && intersection.properties.collisionsWillMove) {
-    grabbedEntity = intersection.entityID;
-    var props = Entities.getEntityProperties(grabbedEntity)
-    prevPosition = props.position;
-    isGrabbing = true;
-    savedGravity = props.gravity;
-    Entities.editEntity(grabbedEntity, {gravity: {x: 0, y: 0, z: 0}});
-    //We need to store entity's current gravity, and then disable it while we move
-
-  }
-
-}
-
-
-function mouseReleaseEvent() {
-  if(isGrabbing){
-    flingObject();
-    Entities.editEntity(grabbedEntity, {gravity: savedGravity});
-  }
-  isGrabbing = false;
-}
-
-function flingObject(){
-  //calculate velocity to give object base on current and previous position
-  entityProps = Entities.getEntityProperties(grabbedEntity);
-
-  flingVelocity = Vec3.subtract(entityProps.position, prevPosition);
-  flingVelocity = Vec3.multiply(flingMultiplier, flingVelocity);
-  flingVelocity.y = 0;
-  Entities.editEntity(grabbedEntity, {velocity: flingVelocity});
-}
-
-function mouseMoveEvent(event) {
-  if (isGrabbing) {
-    entityProps = Entities.getEntityProperties(grabbedEntity);
-    prevPosition = entityProps.position;
-    avatarEntityDistance = Vec3.distance(MyAvatar.position, entityProps.position);
-    finalMoveMultiplier = baseMoveFactor * Math.pow(avatarEntityDistance, 1.5);
-    deltaMouse.x = event.x - prevMouse.x;
-    if(!moveUpDown){
-      deltaMouse.z = event.y - prevMouse.y;
-    } else {
-      deltaMouse.y = (event.y - prevMouse.y) * -1;
+    position: {
+      x: MyAvatar.position.x,
+      y: MyAvatar.position.y - 5,
+      z: MyAvatar.position.z
+    },
+    dimensions: {
+      x: 100,
+      y: 2,
+      z: 100
+    },
+    color: {
+      red: 20,
+      green: 200,
+      blue: 50
     }
-    finalMoveMultiplier = baseMoveFactor * Math.pow(avatarEntityDistance, 1.5);
-    deltaMouse = Vec3.multiply(deltaMouse, finalMoveMultiplier);
-    camYaw = Quat.safeEulerAngles(Camera.getOrientation()).y;
-    dv = Vec3.multiplyQbyV(Quat.fromPitchYawRollDegrees(0, camYaw, 0), deltaMouse);
-    newPosition = Vec3.sum(entityProps.position, dv);
-    Entities.editEntity(grabbedEntity, {
-      position: newPosition
-    });
-  }
-  prevMouse.x = event.x;
-  prevMouse.y = event.y;
-}
-
-function keyReleaseEvent(event){
-  if(event.text === "SHIFT"){
-    moveUpDown = false;
-  }
-}
-
-function keyPressEvent(event){
-  if(event.text === "SHIFT"){
-    moveUpDown = true;
-  }
-}
-
-function cleanup() {
-  Entities.deleteEntity(box);
-  Entities.deleteEntity(box2);
-  Entities.deleteEntity(ground);
+  });
 }
 
 Controller.mouseMoveEvent.connect(mouseMoveEvent);
