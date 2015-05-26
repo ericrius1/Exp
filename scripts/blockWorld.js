67cade31b384bc7e654144542882ae50655c4914
Eric Levin <ericrius1@gmail.com>
2015-05-26 12:59:36 -0700

    planky blocks

--- scripts/blockWorld.js
+++ scripts/blockWorld.js
@@ -1,36 +1,65 @@
+var TILE_SIZE = 7
+var GENERATE_INTERVAL = 50;
+var NUM_ROWS = 10;
+
 var floorTiles = [];
 var blocks = [];
+var blockSpawner;
 
+HIFI_PUBLIC_BUCKET = "http://s3.amazonaws.com/hifi-public/";
 
-var boxHeight = 10;
-var xWidth = 1600
-var zWidth = 1600;
-
-var rowIndex = 0;
-var rowCount = xWidth / boxSize;
-
-var boxSize = 32
-var GENERATE_INTERVAL = 50;
 
 var floorPos = Vec3.sum(MyAvatar.position, {
   x: 0,
-  y: -6,
+  y: -2,
   z: 0
 });
 var x = floorPos.x;
 
-var NUM_ROWS = 2;
-var NUM_COLUMNS = 2;
 var currentRowIndex = 0;
 var currentColumnIndex = 0;
 
 var DROP_HEIGHT = floorPos.y + 5;
-var BLOCK_GRAVITY = {x: 0, y: -1, z: 0};
-var BLOCK_SIZE = {x: 0.2, y: 0.1, z: 0.8};
+var BLOCK_GRAVITY = {
+  x: 0,
+  y: -9,
+  z: 0
+};
+var BLOCK_SIZE = {
+  x: 0.2,
+  y: 0.1,
+  z: 0.8
+};
+
+var bounds = {
+  xMin: floorPos.x,
+  xMax: floorPos.x + (TILE_SIZE * NUM_ROWS) - TILE_SIZE,
+  zMin: floorPos.z,
+  zMax: floorPos.z + (TILE_SIZE * NUM_ROWS) - TILE_SIZE
+};
+
+var screenSize = Controller.getViewportDimensions();
+
+var BUTTON_SIZE = 32;
+var PADDING = 3;
+
+var offButton = Overlays.addOverlay("image", {
+  x: screenSize.x / 2 - BUTTON_SIZE * 2 + PADDING,
+  y: screenSize.y - (BUTTON_SIZE + PADDING),
+  width: BUTTON_SIZE,
+  height: BUTTON_SIZE,
+  imageURL: HIFI_PUBLIC_BUCKET + "images/close.png",
+  color: {
+    red: 255,
+    green: 255,
+    blue: 255
+  },
+  alpha: 1
+});
 
 
 function generateFloor() {
-  for (var z = floorPos.z; currentColumnIndex < NUM_COLUMNS; z += boxSize, currentColumnIndex++) {
+  for (var z = floorPos.z; currentColumnIndex < NUM_ROWS; z += TILE_SIZE, currentColumnIndex++) {
     floorTiles.push(Entities.addEntity({
       type: 'Box',
       position: {
@@ -39,46 +68,65 @@ function generateFloor() {
         z: z
       },
       dimensions: {
-        x: boxSize,
-        y: boxHeight,
-        z: boxSize
+        x: TILE_SIZE,
+        y: 2,
+        z: TILE_SIZE
       },
       color: {
-        red: randFloat(70, 80),
+        red: randFloat(70, 120),
         green: randFloat(70, 71),
         blue: randFloat(70, 80)
-      }
+      },
+      // collisionsWillMove: true
     }));
   }
 
+  currentRowIndex++;
   if (currentRowIndex < NUM_ROWS) {
-    currentRowIndex++;
     currentColumnIndex = 0;
-    x += boxSize;
+    x += TILE_SIZE;
     Script.setTimeout(generateFloor, GENERATE_INTERVAL);
   } else {
     //Once we're done generating floor, drop planky blocks at random points on floor
-    dropBlocks();
+    blockSpawner = Script.setInterval(function() {
+      dropBlock();
+    }, GENERATE_INTERVAL)
   }
 }
 
-function dropBlocks(){
+function dropBlock() {
   var dropPos = floorPos;
   dropPos.y = DROP_HEIGHT;
-  dropBlock(dropPos);
-
-}
-
-function dropBlock(position){
-  print("DROP")
+  dropPos.x = randFloat(bounds.xMin, bounds.xMax);
+  dropPos.z = randFloat(bounds.zMin, bounds.zMax);
   blocks.push(Entities.addEntity({
     type: "Model",
     modelURL: 'http://s3.amazonaws.com/hifi-public/marketplace/hificontent/Games/blocks/block.fbx',
-    position: position,
+    shapeType: 'box',
+    position: dropPos,
     dimensions: BLOCK_SIZE,
-    gravity: BLOCK_GRAVITY,
-    velocity: {x: 0, y: .1, z: 0}
+    collisionsWillMove: true,
+    gravity: {
+      x: 0,
+      y: -9,
+      z: 0
+    },
+    velocity: {
+      x: 0,
+      y: .1,
+      z: 0
+    }
   }));
+}
+
+function mousePressEvent(event){
+  var clickedOverlay = Overlays.getOverlayAtPoint({
+    x: event.x,
+    y: event.y
+  });
+  if (clickedOverlay == offButton) {
+    Script.clearInterval(blockSpawner);
+  } 
 }
 
 generateFloor();
@@ -87,12 +135,12 @@ function destroy() {
   for (var i = 0; i < floorTiles.length; i++) {
     Entities.deleteEntity(floorTiles[i]);
   }
-   for (var i = 0; i < blocks.length; i++) {
+  for (var i = 0; i < blocks.length; i++) {
     Entities.deleteEntity(blocks[i]);
   }
+  Overlays.deleteOverlay(offButton);
+  Script.clearInterval(blockSpawner);
 }
-
-Script.scriptEnding.connect(destroy);
 
 function randFloat(low, high) {
   return Math.floor(low + Math.random() * (high - low));
@@ -101,3 +149,6 @@ function randFloat(low, high) {
 function map(value, min1, max1, min2, max2) {
   return min2 + (max2 - min2) * ((value - min1) / (max1 - min1));
 }
+
+Script.scriptEnding.connect(destroy);
+Controller.mousePressEvent.connect(mousePressEvent);
