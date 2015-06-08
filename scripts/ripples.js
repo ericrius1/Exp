d25acc2abb5556b969980c60bbbd84cb11f79788
Eric Levin <ericrius1@gmail.com>
2015-06-08 16:59:17 -0700

    deleting all entities

--- scripts/ripples.js
+++ scripts/ripples.js
@@ -10,20 +10,32 @@ var grid = [];
 var entities = [];
 
 for (var rowIndex = 0; rowIndex < NUM_ROWS; rowIndex++) {
-  for(var columnIndex = 0; columnIndex < NUM_ROWS; columnIndex++) {
-    if(!grid[rowIndex]) {
+  for (var columnIndex = 0; columnIndex < NUM_ROWS; columnIndex++) {
+    if (!grid[rowIndex]) {
       grid[rowIndex] = [];
     }
     var entity = Entities.addEntity({
       type: 'Box',
-      position: Vec3.sum(center, {x: rowIndex * BOX_SIZE, y: -2, z: columnIndex * BOX_SIZE}),
-      dimensions: {x: BOX_SIZE, y: BOX_SIZE, z: BOX_SIZE},
-      color: hslToRgb({hue: 0.6, sat: 0.5, light: randFloat(0.5, 0.6)})
+      position: Vec3.sum(center, {
+        x: rowIndex * BOX_SIZE,
+        y: -2,
+        z: columnIndex * BOX_SIZE
+      }),
+      dimensions: {
+        x: BOX_SIZE,
+        y: BOX_SIZE,
+        z: BOX_SIZE
+      },
+      color: hslToRgb({
+        hue: 0.6,
+        sat: 0.5,
+        light: randFloat(0.5, 0.6)
+      })
     });
-    grid[rowIndex].push(Entities.addEntitiy({
-      type: 'Box',
-
-    }))
+    grid[rowIndex].push({
+      entity: entity,
+      neighbors: []
+    })
   }
 }
 
@@ -31,9 +43,12 @@ for (var rowIndex = 0; rowIndex < NUM_ROWS; rowIndex++) {
 function update(deleteTime) {}
 
 function cleanup() {
-  cubes.forEach(function(cube) {
-    Entities.deleteEntity(cube);
-  })
+  for(var rowIndex = 0; rowIndex < grid.length; rowIndex++){
+    for(var columnIndex = 0; columnIndex < grid[rowIndex].length; columnIndex++) {  
+      Entities.deleteEntity(grid[rowIndex][columnIndex].entity);
+
+    }
+  }
 }
 
 Script.update.connect(update);
