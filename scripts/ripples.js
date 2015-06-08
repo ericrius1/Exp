2c06174993a7861e4ff48dfef618f7cd82f3dde3
Eric Levin <ericrius1@gmail.com>
2015-06-08 16:52:58 -0700

    adding entities to grid

--- scripts/ripples.js
+++ scripts/ripples.js
@@ -3,36 +3,30 @@ HIFI_PUBLIC_BUCKET = "http://s3.amazonaws.com/hifi-public/";
 Script.include('utilities.js');
 
 var NUM_ROWS = 10;
-var CUBE_SIZE = 2;
-var cubes = [];
-var cubesSettings = [];
+var BOX_SIZE = 1;
+var center = Vec3.sum(MyAvatar.position, Vec3.multiply(BOX_SIZE * 10, Quat.getFront(Camera.getOrientation())));
 
+var grid = [];
+var entities = [];
 
-var center = Vec3.sum(MyAvatar.position, Vec3.multiply(CUBE_SIZE * 10, Quat.getFront(Camera.getOrientation())));
-
-
-for (var x = 0, rowIndex = 0; x < NUM_ROWS * CUBE_SIZE; x += CUBE_SIZE, rowIndex++) {
-  for (var z = 0, columnIndex = 0; z < NUM_ROWS * CUBE_SIZE; z += CUBE_SIZE, columnIndex++) {
-
-    var baseHeight = map(columnIndex + 1, 1, NUM_ROWS, -CUBE_SIZE * 2, -CUBE_SIZE);
-    var relativePosition = {
-      x: x,
-      y: baseHeight,
-      z: z
-    };
-    var position = Vec3.sum(center, relativePosition);
-    cubes.push(Entities.addEntity({
+for (var rowIndex = 0; rowIndex < NUM_ROWS; rowIndex++) {
+  for(var columnIndex = 0; columnIndex < NUM_ROWS; columnIndex++) {
+    if(!grid[rowIndex]) {
+      grid[rowIndex] = [];
+    }
+    var entity = Entities.addEntity({
       type: 'Box',
-      position: position,
-      dimensions: {
-        x: CUBE_SIZE,
-        y: CUBE_SIZE,
-        z: CUBE_SIZE
-      }
-    }));
-  }
+      position: Vec3.sum(center, {x: rowIndex * BOX_SIZE, y: -2, z: columnIndex * BOX_SIZE}),
+      dimensions: {x: BOX_SIZE, y: BOX_SIZE, z: BOX_SIZE},
+      color: hslToRgb({hue: 0.6, sat: 0.5, light: randFloat(0.5, 0.6)})
+    });
+    grid[rowIndex].push(Entities.addEntitiy({
+      type: 'Box',
 
+    }))
+  }
 }
+
 
 function update(deleteTime) {}
 
