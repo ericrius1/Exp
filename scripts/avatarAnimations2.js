e38666f0a0db6f94da647e905991a80a49ddd8b9
ericrius1 <ericrius1@gmail.com>
2015-07-30 14:51:31 -0700

    frame still counter to prevent incorrect state change

--- scripts/avatarAnimations2.js
+++ scripts/avatarAnimations2.js
@@ -5,7 +5,6 @@ Script.include("tween.js", function() {
 
 function init() {
 
-	var stepLeftAnimation = "https://hifi-public.s3.amazonaws.com/ozan/animations/fightclub_bot_anims/side_step_left_inPlace.fbx";
 
 	var MOVE_THRESHOLD = 0.001;
 	var previousPosition = MyAvatar.position;
@@ -17,6 +16,11 @@ function init() {
 	var numFrames = 24;
 
 	var direction;
+
+	var stillFramesCounter = 0;
+	//How many frames to wait before we are at idle. 
+	//This prevents changing state when we're quickly changing direction in a walk, sidestep, etc
+	var STILL_FRAMES_THRESHOLD = 10;
 
 	var sideStepAnimation = {
 		url: "https://hifi-public.s3.amazonaws.com/ozan/animations/fightclub_bot_anims/side_step_right_inPlace.fbx",
@@ -30,6 +34,10 @@ function init() {
 		frameIncrementFactor: 1
 	}
 
+	var turnAnimation = {
+
+	}
+
 	var idleAnimation = {
 		url: "https://hifi-public.s3.amazonaws.com/ozan/animations/fightclub_bot_anims/idle.fbx"
 	}
@@ -37,23 +45,18 @@ function init() {
 	var nextFrame;
 	var frameIncrement;
 
-
-	// MyAvatar.startAnimation(walkAnimation, 24, 1, true, false);
-
-	Script.setInterval(slowUpdate, 40);
-
+	Script.setInterval(slowUpdate, 40); //Avoid bug where dPosition is 0 with fast update loop even when avatar is moving
 
 	function update() {
 		TWEEN.update();
 	}
 
 	function finishQuickly(callback) {
+		print('THRESHOLD REACHEd')
 		var frameIndex = MyAvatar.getAnimationDetails(currentAnimation.url).frameIndex;
-		print("frame index "  + JSON.stringify(frameIndex));
 		var finishTween = new TWEEN.Tween({frameIndex : nextFrame}).
 		to({frameIndex: nextFrame > currentAnimation.numFrames/2 ? currentAnimation.numFrames : 0}, 500).
 		onUpdate(function() {
-			// print(this.function() {};rameIndex)
 			MyAvatar.startAnimation(currentAnimation.url, 24, 1, false, false, this.frameIndex, this.frameIndex + .1);
 		}).start()
 
@@ -71,43 +74,54 @@ function init() {
 
 		previousPosition = MyAvatar.position;
 		if (Vec3.length(dPosition) < MOVE_THRESHOLD) {
+			stillFramesCounter++;
 			//If we're barely moving just idle and return;
-			if (avatarState !== "idling") {
+			print("FRAME COUNTER:  " + stillFramesCounter	)
+			if (avatarState !== "idling" && stillFramesCounter >= STILL_FRAMES_THRESHOLD) {
 				  avatarState = "idling";
 				//We're in another animation, so finish this animation quickly and then start idle animation on complete
 				finishQuickly(function(){
-					//must stop current animation for frameIndex is fucked
+				//must stop current animation for frameIndex is fucked
 				  MyAvatar.stopAnimation(currentAnimation.url);
 				  MyAvatar.startAnimation(idleAnimation.url, 24, 1, true, false);
 				  currentAnimation = idleAnimation;
 				  currentFrame = 0;
+			
 				});
 			} 
 			return;
+		} else {
+		    stillFramesCounter = 0;
+			if (Math.abs(dPosition.x) > Math.abs(dPosition.z) * 5) {
+				// if we're moving more than double side to side then forward, sidestep
+				avatarState = "sideStepping"
+				sideStep();
+			} else {
+				avatarState = "walking"
+				walk();
+			}
+
+			
 		}
 
-		if (Math.abs(dPosition.x) > Math.abs(dPosition.z)) {
-			// if we're moving more side to side then forward, sidestep
-			avatarState = "sideStepping"
-			sideStep();
-		} else {
-			avatarState = "walking"
-			walk();
-		}
 	}
 
 	function walk() {
 		MyAvatar.startAnimation(walkAnimation.url, 24, 1, false, false, currentFrame, nextFrame);
 		currentAnimation = walkAnimation;
 
-
-		direction = dPosition.z > 0 ? -1 : 1
+		direction = dPosition.z < 0 ? 1 : -1
 		frameIncrement = direction * walkAnimation.frameIncrementFactor;
 		currentFrame = currentFrame + frameIncrement
 		nextFrame = currentFrame + frameIncrement;
 		if (currentFrame > walkAnimation.numFrames) {
 			currentFrame = 0;
 			nextFrame = frameIncrement;
+		}
+		print("WALK")
+		if (currentFrame < 0) {
+			currentFrame = walkAnimation.numFrames;
+			nextFrame = walkAnimation.numFrames + frameIncrement;
 		}
 
 
@@ -128,7 +142,7 @@ function init() {
 		}
 		if (currentFrame < 0) {
 			currentFrame = sideStepAnimation.numFrames;
-			sideStepAnimation.numFrames - frameIncrement;
+			nextFrame = sideStepAnimation.numFrames + frameIncrement;
 		}
 
 
