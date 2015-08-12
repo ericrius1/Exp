4e64cb8badb1f77dd7bddd383eda6b633274bfc2
ericrius1 <ericrius1@gmail.com>
2015-08-12 15:27:53 -0700

    building particle example

--- scripts/partiArt.js
+++ scripts/partiArt.js
@@ -7,8 +7,25 @@ var RIGHT_TRIGGER = 1;
 var prevRightTriggerValue = 0;
 var TRIGGER_THRESHOLD = 0.2;
 var triggerHeld = false;
-var rightControllerId = 3;
-var leftControllerId = 4;
+
+var RIGHT = 1;
+var LEFT = 0;
+var RIGHT_BUTTON_1 = 7
+var RIGHT_BUTTON_2 = 8
+var RIGHT_BUTTON_3 = 9;
+var RIGHT_BUTTON_4 = 10
+var LEFT_BUTTON_1 = 1;
+var LEFT_BUTTON_2 = 2;
+var LEFT_BUTTON_3 = 3;
+var LEFT_BUTTON_4 = 4;
+var rightPalm = 2 * RIGHT;
+var rightTip = 2 * RIGHT + 1;
+var leftPalm = 2 * LEFT;
+var leftTip = 2 * LEFT + 1;
+
+var particleRadius = 0.01;
+
+var HIDDEN_POSITION = {x: -100, y: -100, z: -100};
 
 var colorPalette = [{
 	red: 0,
@@ -32,15 +49,14 @@ var emitter;
 createEmitter();
 
 
-function computeWorldPoint(pickRay) {
-	var addVector = Vec3.multiply(Vec3.normalize(pickRay.direction), EMITTER_DISTANCE);
-	return Vec3.sum(pickRay.origin, addVector);
-}
-
 function update() {
 	updateControllerState();
 	if (triggerHeld) {
-		Entities.editEntity(emitter, {position: Controller.getSpatialControlPosition(rightControllerId)});
+		Entities.editEntity(emitter, {
+			position: Controller.getSpatialControlPosition(rightTip),
+			emitVelocity: Controller.getSpatialControlNormal(rightTip),
+			particleRadius: particleRadius
+		});
 	}
 }
 
@@ -50,6 +66,7 @@ function updateControllerState() {
 		triggerHeld = true;
 	} else if (rightTriggerValue < TRIGGER_THRESHOLD && prevRightTriggerValue > TRIGGER_THRESHOLD && triggerHeld) {
 		triggerHeld = false;
+		Entities.editEntity(emitter, {position: HIDDEN_POSITION});
 	}
 
 	prevRightTriggerValue = rightTriggerValue;
@@ -74,25 +91,12 @@ function createEmitter(position) {
 	emitter = Entities.addEntity({
 		type: "ParticleEffect",
 		animationSettings: animationSettings,
+		position: HIDDEN_POSITION,
 		textures: "https://raw.githubusercontent.com/ericrius1/SantasLair/santa/assets/smokeparticle.png",
 		emitRate: 100,
-		emitVelocity: {
-			x: 0,
-			y: 1,
-			z: 0
-		},
-		velocitySpread: {
-			x: 1,
-			y: 0.5,
-			z: 1
-		},
-		emitAcceleration: {
-			x: 0,
-			y: -1,
-			z: 1
-		},
+		velocitySpread: {x: .1, y: .1, z: .1},
 		color: colorPalette[0],
-		lifespan: 100,
+		lifespan: 40,
 	});
 
 	emitters.push(emitter);
