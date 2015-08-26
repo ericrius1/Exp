5e2f3cd6de4ce945e96d200702e6137b3e10fe0e
ericrius1 <ericrius1@gmail.com>
2015-08-26 10:39:16 -0700

    cleanup

--- scripts/sprayPaint.js
+++ scripts/sprayPaint.js
@@ -17,9 +17,9 @@ var MIN_POINT_DISTANCE = 0.01;
 var MAX_POINTS_PER_LINE = 40;
 
 var ZERO_VEC = {
-	x: 0,
-	y: 0,
-	z: 0
+    x: 0,
+    y: 0,
+    z: 0
 };
 
 var holdingTrigger = false;
@@ -48,27 +48,27 @@ var prevRightHandGrabValue = 0;
 // });
 
 var whiteboard = Entities.addEntity({
-	type: "Box",
-	position: center,
-	dimensions: {
-		x: 2,
-		y: 1.5,
-		z: .01
-	},
-	rotation: orientationOf(Vec3.subtract(MyAvatar.position, center)),
-	color: {
-		red: 250,
-		green: 250,
-		blue: 250
-	},
+    type: "Box",
+    position: center,
+    dimensions: {
+        x: 2,
+        y: 1.5,
+        z: .01
+    },
+    rotation: orientationOf(Vec3.subtract(MyAvatar.position, center)),
+    color: {
+        red: 250,
+        green: 250,
+        blue: 250
+    },
 });
 
 
 var animationSettings = JSON.stringify({
-	fps: 30,
-	loop: true,
-	firstFrame: 1,
-	lastFrame: 10000
+    fps: 30,
+    loop: true,
+    firstFrame: 1,
+    lastFrame: 10000
 });
 
 var stopSetting = JSON.stringify({
@@ -80,168 +80,168 @@ var startSetting = JSON.stringify({
 
 
 var paintStream = Entities.addEntity({
-	type: "ParticleEffect",
-	animationSettings: animationSettings,
-	position: center,
-	textures: "https://raw.githubusercontent.com/ericrius1/SantasLair/santa/assets/smokeparticle.png",
-	emitVelocity: ZERO_VEC,
-	velocitySpread: {
-		x: .05,
-		y: .05,
-		z: .05
-	},
-	emitAcceleration: ZERO_VEC,
-	emitRate: 100,
-	color: {
-		red: 170,
-		green: 20,
-		blue: 150
-	},
-	lifespan: 0.2,
+    type: "ParticleEffect",
+    animationSettings: animationSettings,
+    position: center,
+    textures: "https://raw.githubusercontent.com/ericrius1/SantasLair/santa/assets/smokeparticle.png",
+    emitVelocity: ZERO_VEC,
+    emitAcceleration: ZERO_VEC,
+    velocitySpread: {x: .02, y:.02, z: 0.02},
+    emitRate: 100,
+    particleRadius: 0.01,
+    color: {
+        red: 170,
+        green: 20,
+        blue: 150
+    },
+    lifespan: 5,
 });
 
 
 
 function update() {
-	updateControllerState();
+    updateControllerState();
 
-	if (!holdingTrigger) {
-		return;
-	}
+    if (!holdingTrigger) {
+        return;
+    }
 
-	var origin = MyAvatar.getRightPalmPosition();
-	var direction = Controller.getSpatialControlNormal(RIGHT_TIP);
-	Entities.editEntity(paintStream, {
-		position: origin,
-		emitVelocity: Vec3.multiply(direction, 5)
-	});
-	//move origin of ray a bit away from hand so the ray doesnt hit emitter
-	origin = Vec3.sum(origin, direction);
-	var intersection = getEntityIntersection(origin, direction);
-	if (intersection.intersects && intersection.distance < DISTANCE_THRESHOLD) {
-		//get normal
-		// print("PROPS	 " + JSON.stringify(intersection))
+    var origin = MyAvatar.getRightPalmPosition();
+    var direction = Controller.getSpatialControlNormal(RIGHT_TIP);
+    Entities.editEntity(paintStream, {
+        position: origin,
+        emitVelocity: Vec3.multiply(direction, 5)
+    });
+    //move origin of ray a bit away from hand so the ray doesnt hit emitter
+    origin = Vec3.sum(origin, direction);
+    var intersection = getEntityIntersection(origin, direction);
+    if (intersection.intersects && intersection.distance < DISTANCE_THRESHOLD) {
+        //get normal
+        // print("PROPS	 " + JSON.stringify(intersection))
         //Add a little delay to let particles hit surface
-        (function(){
+        // var position = intersection.intersection;
+        // var normal = Vec3.multiply(-1, Quat.getFront(intersection.properties.rotation));
+        // paint(position, normal);
+        (function() {
             var position = intersection.intersection;
-            var normal =  Vec3.multiply(-1, Quat.getFront(intersection.properties.rotation));
+            var normal = Vec3.multiply(-1, Quat.getFront(intersection.properties.rotation));
             Script.setTimeout(function() {
-                if(!holdingTrigger){
+                if (!holdingTrigger) {
                     return;
                 }
-    		  paint(position , normal);
-            }, 200)
+                paint(position, normal);
+            }, 300)
         })();
-	}
+    }
 }
 
 
 function paint(position, normal) {
-	if (!painting) {
-        print("NEW LINE")
-		newStroke(position)
-		painting = true;
-	}
+    if (!painting) {
+        // print("NEW LINE")
+        newStroke(position)
+        painting = true;
+    }
 
-	if(strokePoints.length > MAX_POINTS_PER_LINE) {
-		painting = false;
-		return;
-	}
-	var localPoint = Vec3.subtract(position, strokeBasePosition);
+    if (strokePoints.length > MAX_POINTS_PER_LINE) {
+        painting = false;
+        return;
+    }
+    var localPoint = Vec3.subtract(position, strokeBasePosition);
     //Move to stroke a bit forward along the normal so it doesnnt zfight with mesh its drawing on
-    localPoint = Vec3.sum(localPoint, Vec3.multiply(normal, .01))   ;
-	if ( strokePoints.length > 0 && Vec3.distance(localPoint, strokePoints[strokePoints.length - 1]) < MIN_POINT_DISTANCE) {
-		//Need a minimum distance to avoid binormal NANs
-		return;
-	}
-	strokePoints.push(localPoint);
-	strokeNormals.push(normal);
-	strokeWidths.push(STROKE_WIDTH);
-	Entities.editEntity(currentStroke, {
-		linePoints: strokePoints,
-		normals: strokeNormals,
-		strokeWidths: strokeWidths
-	});
+    localPoint = Vec3.sum(localPoint, Vec3.multiply(normal, .1));
+    if (strokePoints.length > 0 && Vec3.distance(localPoint, strokePoints[strokePoints.length - 1]) < MIN_POINT_DISTANCE) {
+        //Need a minimum distance to avoid binormal NANs
+        return;
+    }
+    strokePoints.push(localPoint);
+    strokeNormals.push(normal);
+    strokeWidths.push(STROKE_WIDTH);
+    Entities.editEntity(currentStroke, {
+        linePoints: strokePoints,
+        normals: strokeNormals,
+        strokeWidths: strokeWidths
+    });
 }
 
 function newStroke(position) {
-	strokeBasePosition = position;
-	currentStroke = Entities.addEntity({
-		position: position,
-		type: "PolyLine",
-		color: {
-			red: randInt(1, 250),
-			green: randInt(1,250),
-			blue: randInt(1, 250)
-		},
-		dimensions: {
-			x: 5,
-			y: 5,
-			z: 5
-		},
-		lifetime: 100
-	});
-	strokePoints = [];
-	strokeNormals = [];
-	strokeWidths = [];
+    strokeBasePosition = position;
+    currentStroke = Entities.addEntity({
+        position: position,
+        type: "PolyLine",
+        color: {
+            red: randInt(160, 250),
+            green: randInt(10, 20),
+            blue: randInt(190, 250)
+        },
+        dimensions: {
+            x: 5,
+            y: 5,
+            z: 5
+        },
+        lifetime: 100
+    });
+    strokePoints = [];
+    strokeNormals = [];
+    strokeWidths = [];
 
     strokes.push(currentStroke);
 
 }
 
 function getEntityIntersection(origin, direction) {
-	var pickRay = {
-		origin: origin,
-		direction: direction
-	};
-	return Entities.findRayIntersection(pickRay, true);
+    var pickRay = {
+        origin: origin,
+        direction: direction
+    };
+    return Entities.findRayIntersection(pickRay, true);
 }
 
 function updateControllerState() {
-	rightHandGrabValue = Controller.getActionValue(rightHandGrabAction);
-	if (rightHandGrabValue > TRIGGER_THRESHOLD && prevRightHandGrabValue < TRIGGER_THRESHOLD) {
-		holdingTrigger = true;
+    rightHandGrabValue = Controller.getActionValue(rightHandGrabAction);
+    if (rightHandGrabValue > TRIGGER_THRESHOLD && prevRightHandGrabValue < TRIGGER_THRESHOLD) {
+        holdingTrigger = true;
         Entities.editEntity(paintStream, {
             animationSettings: startSetting
         });
-	} else if (rightHandGrabValue < TRIGGER_THRESHOLD && prevRightHandGrabValue > TRIGGER_THRESHOLD) {
-		holdingTrigger = false;
-		painting = false;
+    } else if (rightHandGrabValue < TRIGGER_THRESHOLD && prevRightHandGrabValue > TRIGGER_THRESHOLD) {
+        holdingTrigger = false;
+        painting = false;
         Entities.editEntity(paintStream, {
             animationSettings: stopSetting
         })
-	}
-	prevRightHandGrabValue = rightHandGrabValue
+    }
+    prevRightHandGrabValue = rightHandGrabValue
 }
 
 
 function orientationOf(vector) {
-	var Y_AXIS = {
-		x: 0,
-		y: 1,
-		z: 0
-	};
-	var X_AXIS = {
-		x: 1,
-		y: 0,
-		z: 0
-	};
+    var Y_AXIS = {
+        x: 0,
+        y: 1,
+        z: 0
+    };
+    var X_AXIS = {
+        x: 1,
+        y: 0,
+        z: 0
+    };
 
-	var theta = 0.0;
+    var theta = 0.0;
 
-	var RAD_TO_DEG = 180.0 / Math.PI;
-	var direction, yaw, pitch;
-	direction = Vec3.normalize(vector);
-	yaw = Quat.angleAxis(Math.atan2(direction.x, direction.z) * RAD_TO_DEG, Y_AXIS);
-	pitch = Quat.angleAxis(Math.asin(-direction.y) * RAD_TO_DEG, X_AXIS);
-	return Quat.multiply(yaw, pitch);
+    var RAD_TO_DEG = 180.0 / Math.PI;
+    var direction, yaw, pitch;
+    direction = Vec3.normalize(vector);
+    yaw = Quat.angleAxis(Math.atan2(direction.x, direction.z) * RAD_TO_DEG, Y_AXIS);
+    pitch = Quat.angleAxis(Math.asin(-direction.y) * RAD_TO_DEG, X_AXIS);
+    return Quat.multiply(yaw, pitch);
 }
 
 function cleanup() {
-	Entities.deleteEntity(whiteboard);
-	Entities.deleteEntity(paintStream);
+    Entities.deleteEntity(whiteboard);
+    Entities.deleteEntity(paintStream);
 
-    strokes.forEach(function(stroke){
+    strokes.forEach(function(stroke) {
         Entities.deleteEntity(stroke);
     });
 }
@@ -250,9 +250,9 @@ Script.scriptEnding.connect(cleanup);
 Script.update.connect(update);
 
 function randFloat(min, max) {
-  return Math.random() * (max - min) + min;
+    return Math.random() * (max - min) + min;
 }
 
 function randInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+    return Math.floor(Math.random() * (max - min)) + min;
 }
